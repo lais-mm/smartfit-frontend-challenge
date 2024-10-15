4420f37dce09d7a6372fdde472701f77eb6a8e04
LAIS <lais-mm@outlook.com>
2024-10-15 20:53:02 -0300

    feat: add form component style

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+  results= [];
+  
+
+}
