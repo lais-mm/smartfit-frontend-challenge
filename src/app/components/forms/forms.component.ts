84cccf8dc7ad6936664fb5725b2225497ba1a315
LAIS <lais-mm@outlook.com>
2024-10-24 20:35:16 -0300

    feat: create filter units service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { FilterUnitsService } from '../../services/filter-units.service';
+
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -13,7 +16,10 @@ export class FormsComponent implements OnInit{
   filteredResults: Location [] = [];
   formGroup!: FormGroup;
 
-  constructor(private  formBuilder: FormBuilder, private unitService: GetUnitsService) {}
+  constructor(
+    private formBuilder: FormBuilder, 
+    private unitService: GetUnitsService, 
+    private filterUnitsService: FilterUnitsService ) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
@@ -21,18 +27,19 @@ export class FormsComponent implements OnInit{
       showClosed: true
     })
     this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     });
   }
 
+
+
   onSubmit(): void{
-    if(!this.formGroup.value.showClosed){
-      this.filteredResults = this.results.filter(location => location.opened === true)
-    }else{
-      this.filteredResults = this.results
-    }
+    let {showClosed, hour} = this.formGroup.value
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour)
+    this.unitService.setFilteredUnits(this.filteredResults)
   }
+
   
   onClean(): void{
     this.formGroup.reset();
